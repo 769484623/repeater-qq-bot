d7e68c37b2483d58e8bf4130984ac84ee28afc90
LNStar <769484623@qq.com>
2020-04-04 22:17:15 +0800

    上线下线欢迎词 & 复读 & 自动复读

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 import nonebot
 import config
+import asyncio
+
+from plugins.auto_repeater import send_exit_msg, send_start_msg
+
+loop = asyncio.get_event_loop()
 
 nonebot.init(config)
-nonebot.load_builtin_plugins()
+nonebot.load_plugin('plugins.auto_repeater')
+
+# 复读姬上线问候语
+loop.run_until_complete(asyncio.wait([send_start_msg('复读姬援交回来啦~功能正常使用~', [559553404])]))
+
 nonebot.run(host='0.0.0.0', port=10240)
+
+# 复读姬退出问候语
+loop.run_until_complete(asyncio.wait([send_exit_msg('复读姬援交去啦~接下来功能将不可用~')]))
+
+loop.close()
--- plugins/auto_repeater.py
+++ plugins/auto_repeater.py
@@ -1,9 +1,51 @@
-from nonebot import on_command, CommandSession
+from nonebot import on_command, CommandSession, get_bot
 from nonebot import on_natural_language, NLPSession, IntentCommand
+import logging
+
+last_msg_of_qq_groups = {}
 
 
-@on_natural_language(keywords={'天气'})
+async def send_start_msg(start_msg, qq_group_list: list = None):
+    if qq_group_list is None:
+        qq_group_list = last_msg_of_qq_groups
+    bot = get_bot()
+    for group in qq_group_list:
+        await bot.send_group_msg(group_id=int(group), message=start_msg)
+
+
+async def send_exit_msg(exit_msg, qq_group_list: list = None):
+    if qq_group_list is None:
+        qq_group_list = last_msg_of_qq_groups
+    bot = get_bot()
+    for group in qq_group_list:
+        await bot.send_group_msg(group_id=int(group), message=exit_msg)
+    logging.getLogger('nonebot').info('Bot Exit')
+
+
+@on_command('群消息复读')
+async def auto_repeater(session: CommandSession):
+    if session.ctx['message_type'] == 'group':
+        qq_group_id = session.ctx['group_id']
+        msg = session.ctx['raw_message']
+        last_msg = last_msg_of_qq_groups.get(qq_group_id)
+        if last_msg is not None:
+            is_the_msg_the_same = msg == last_msg['msg']
+            if is_the_msg_the_same:
+                print(last_msg)
+                if not last_msg['is_repeated']:
+                    last_msg['is_repeated'] = True
+                    await session.send(msg)
+                return
+        last_msg_of_qq_groups[qq_group_id] = {'msg': msg, 'is_repeated': False}
+
+
+@on_command('复读')
+async def repeater(session: CommandSession):
+    await session.send(session.current_arg_text)
+
+
+# 监听所有非命令聊天
+@on_natural_language(only_to_me=False)
 async def _(session: NLPSession):
-    # 返回意图命令，前两个参数必填，分别表示置信度和意图命令名
-    print(session)
-    return IntentCommand(90.0, 'weather')
+    # 复读机只处理QQ群消息
+    return IntentCommand(90, '群消息复读')
